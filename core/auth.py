947413d8d095fb880bdd12b42245090d88074eca
Konstantin <wetarmor@gmail.com>
2019-10-08 20:23:22 +0300

    Updated redirection. Added handling unauthorized user

--- core/auth.py
+++ core/auth.py
@@ -3,6 +3,7 @@ from flask_login import current_user, login_user, logout_user
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.urls import url_parse
 
+from core import login_manager
 from core.forms import LoginForm, RegistrationForm
 from core.models import db, User
 
@@ -12,7 +13,7 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('message.send'))
 
     form = RegistrationForm()
     if form.validate_on_submit():
@@ -26,7 +27,7 @@ def register():
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('message.send'))
 
     form = LoginForm()
     if form.validate_on_submit():
@@ -39,12 +40,19 @@ def login():
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
+            next_page = url_for('message.send')
         return redirect(next_page)
-    return render_template('auth/login.html', title='хуй пизда', form=form)
+    return render_template('auth/login.html', title='Login', form=form)
 
 
 @bp.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('message.send'))
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    """Redirect unauthorized users to Login page."""
+    flash('You must be logged in to view that page.')
+    return redirect(url_for('auth.login'))
